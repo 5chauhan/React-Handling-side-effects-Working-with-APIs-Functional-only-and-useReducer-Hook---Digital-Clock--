81081d1f39c220196a709850664796354f1c1095
Sourabh Chauhan <isurabhkumar@gmail.com>
2022-05-11 23:40:10 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,43 +1,53 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState, useEffect } from 'react';
 import '../styles/App.css';
 
-class App extends Component {
+function getCurrentTime() {
+  const date = new Date();
+  let hours = date.getHours();
+  let amOrPm = 'AM';
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            date : new Date()
-        }
+  if (isPM(hours)) {
+    amOrPm = 'PM';
+    hours = hours - 12;
+  }
+  const mins = date.getMinutes().toString().padStart(2, '0');
+  const secs = date.getSeconds().toString().padStart(2, '0');
 
-        this.tick = this.tick.bind(this);
-    }
-
-    componentDidMount() {
-        this.timerID = setInterval(this.tick, 1000)
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.timerID);
-    }
-
-    tick() {
-        // console.log("state is not updated")
-        this.setState({
-            date: new Date()
-        }, 
-        // () => console.log("state is updated")
-        )
-    }
-    
-    render() {
-
-        return(
-            <div className="Clock">
-               <h3 id="time">{this.state.date.toLocaleTimeString()}</h3>
-            </div>
-        )
-    }
+  return `${hours}:${mins}:${secs} ${amOrPm}`;
 }
 
+function isPM(hours) {
+  if (hours > 12) {
+    return true;
+  }
+  return false;
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { time: getCurrentTime() };
+  }
+  componentDidMount() {
+    this.intervalId = setInterval(() => {
+      this.setState({ time: getCurrentTime() });
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
+  }
+  render() {
+    return (
+      <div className="Clock">
+        <h3 id="time">{this.state.time}</h3>
+      </div>
+    );
+  }
+}
+
+function Clock() {
+  useEffect(function () {}, []);
+}
 
 export default App;
